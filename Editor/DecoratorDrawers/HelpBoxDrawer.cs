285c0d247196e53481236beaa31470b2641a6c4f
CaseyHofland <hofland.casey@gmail.com>
2024-03-23 13:29:00 +0100

    Fix HelpBox styling

--- Editor/DecoratorDrawers/HelpBoxDrawer.cs
+++ Editor/DecoratorDrawers/HelpBoxDrawer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -14,9 +15,19 @@ namespace Cubusky.Editor
             return new HelpBox(helpBox.text, helpBox.messageType);
         }
 #else
+        public override float GetHeight()
+        {
+            var helpBox = attribute as HelpBoxAttribute;
+            var style = GUI.skin != null ? GUI.skin.GetStyle("helpbox") : null;
+            var minHeight = helpBox.messageType != HelpBoxMessageType.None ? 40f : 0f;
+            var height = style == null ? base.GetHeight() : style.CalcHeight(new(helpBox.text), EditorGUIUtility.currentViewWidth) + 4f;
+            return MathF.Max(minHeight, height);
+        }
+
         public override void OnGUI(Rect position)
         {
             var helpBox = attribute as HelpBoxAttribute;
+            position.height -= 2f;
             EditorGUI.HelpBox(position, helpBox.text, (MessageType)helpBox.messageType);
         }
 #endif
